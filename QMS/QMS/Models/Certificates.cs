68912d67ad0ef17a01031e59a029b3b14ad1ed8f
Pushpendra <pushpendra.shukla@dotsquares.com>
2025-11-06 13:07:33 +0530

    project setup

---
+++ QMS/QMS/Models/Certificates.cs
@@ -0,0 +1,11 @@
+﻿namespace QMS.Models
+{
+    public class Certificates
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Slug { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ QMS/QMS/Models/CertificationQuestion.cs
@@ -0,0 +1,24 @@
+﻿namespace QMS.Models
+{
+    public class CertificationQuestion
+    {
+        public int CertificationId { get; set; }
+        public string CertificationName { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public List<Option> Options { get; set; }
+        public int CorrectOptionId { get; set; }
+        public string CorrectOptionText { get; set; }
+    }
+
+    public class Option
+    {
+        public int OptionId { get; set; }
+        public string OptionText { get; set; }
+    }
+}
